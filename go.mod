a829b4428d04c3356fd9ec588afc1bca05937319
Ivan <vanzag1738@ya.ru>
2025-11-22 19:05:37 +0300

    Unit tested Kafka producer

--- go.mod
+++ go.mod
@@ -11,12 +11,15 @@ require (
 	github.com/brianvoe/gofakeit/v7 v7.7.1
 	github.com/redis/go-redis/v9 v9.14.0
 	github.com/segmentio/kafka-go v0.4.49
+	github.com/stretchr/testify v1.11.1
 	github.com/swaggo/http-swagger v1.3.4
+	go.uber.org/mock v0.6.0
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
@@ -26,11 +29,13 @@ require (
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
 	github.com/swaggo/swag v1.16.6 // indirect
-	golang.org/x/mod v0.17.0 // indirect
-	golang.org/x/net v0.38.0 // indirect
-	golang.org/x/sync v0.12.0 // indirect
-	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	golang.org/x/mod v0.27.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/tools v0.36.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/kafka/producer_test.go
@@ -0,0 +1,63 @@
+package kafka
+
+import (
+	"context"
+	"errors"
+	"orders/internal/mocks"
+	"testing"
+
+	"github.com/segmentio/kafka-go"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+)
+
+func TestWriteMessage(t *testing.T) {
+	t.Run("Successful write", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mockProducer := mocks.NewMockMessagesProducer(ctrl)
+
+		ctx := context.Background()
+		msg := []byte("Hello there, this is a test for Kafka writer !")
+		expectedMsg := kafka.Message{
+			Key:   nil,
+			Value: msg,
+		}
+
+		mockProducer.EXPECT().
+			WriteMessages(ctx, gomock.Any()).
+			DoAndReturn(func(ctx context.Context, msgs ...kafka.Message) error {
+				if len(msgs) != 1 {
+					t.Errorf("Expected 1 message, got %d", len(msgs))
+				}
+				if string(msgs[0].Value) != string(expectedMsg.Value) {
+					t.Errorf("Message content missmatch")
+				}
+				return nil
+			}).
+			Times(1)
+
+		err := WriteMessage(mockProducer, ctx, msg)
+		assert.NoError(t, err, "WriteMessage should not return an error on successful mock call")
+	})
+
+	t.Run("Failed write", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mockProducer := mocks.NewMockMessagesProducer(ctrl)
+		ctx := context.Background()
+		msg := []byte("Another test for error check")
+		expectedError := errors.New("Simulated Kafka error")
+
+		mockProducer.EXPECT().
+			WriteMessages(gomock.Any(), gomock.Any()).
+			Return(expectedError).
+			Times(1)
+
+		err := WriteMessage(mockProducer, ctx, msg)
+		assert.Error(t, err, "WriteMessage should return an error on failed mock call")
+		assert.Equal(t, expectedError, err, "Returned error should match expected one")
+	})
+}
